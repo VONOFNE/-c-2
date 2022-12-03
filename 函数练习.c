3ed24f4f10af6aa66f2d7e972d4a59935adb4527
俱乐杰 <2540930286@qq.com>
2022-12-03 16:16:42 +0800

    函数练习：判断素数和判断闰年

--- 函数练习.c
+++ 函数练习.c
@@ -5,20 +5,43 @@
  #include<time.h>
  #include<math.h>
 
+/*判断素数*/
+int findprimenum(int x)
+{
+    int z = 0;
+    for(z = 2;z < x; z++ )
+    {
+        if(x%z == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 /*互换函数（利用地址对主函数的变量进行操作）*/
-void swap(int* pa,int* pb)
+void swap(int* pa,int* pb)//
 {
     int z = 0;
     z = *pa;
     *pa = *pb;
     *pb = z;
 }
+
+/*判断闰年*/
+int is_leapyear(int x)
+{
+    return ((x % 4 == 0 && x%100 == 0) || (x %400 == 0));
+}
+
  int main ()
  {
-    int a = 1;
-    int b = 2;
-    printf("a = %d,b = %d\n",a,b);
-    swap(&a,&b);
-    printf("a = %d,b = %d\n",a,b);
+    int i = 0;
+    printf("Please input the year you want to test:");
+    scanf("%d",&i);
+    if(is_leapyear(i) == 1)
+    printf("yes,it's.");
+    else
+    printf("No,it's not");
     return 0;
  }
